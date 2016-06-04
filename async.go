6fd55718116fee24a7390007703c822de88ff300
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-04 20:05:58 +0200

    fmt

--- async.go
+++ async.go
@@ -1,8 +1,9 @@
+// Package async help to execute function in parallel.
 package async
 
 import (
-  "errors"
-  // "fmt"
+	"errors"
+	// "fmt"
 )
 
 // func indicating task completion
@@ -17,83 +18,83 @@ type Task func(Done)
 // If Done is invoked multiple times, it is ignored.
 // Index is the index of the task.
 type Result struct {
-  done  bool
-  Err   error
-  Index int
+	done  bool
+	Err   error
+	Index int
 }
 
 // Run multiple task with a limit of concurrent execution.
 // []Result length is always equal to the number of task to execute.
 // []Result returns completion information for each Task.
 // if limit < 1, it returns an error.
-func ParallelLimit (limit int, handlers []Task) ([]Result, error) {
-  results := make([]Result, len(handlers))
-  if limit<1 {
-    return results, errors.New("Limit must be greater than 0")
-  }
-  if len(handlers)<1 {
-    return results, nil
-  }
-  out := make(chan Result)
-  in := make(chan Task, limit)
-  for index, t := range handlers {
-    go func (index int, t Task) {
-      in <- t
-      t(func (err error) {
-        if results[index].done==false {
-          out <- Result{Index: index, Err: err, done: true}
-          <- in
-        }
-      })
-    }(index, t)
-  }
-  for res := range out {
-    results[res.Index] = res
-    if hasEnded(results) {
-      close(out)
-      close(in)
-    }
-  }
-  return results, nil
+func ParallelLimit(limit int, handlers []Task) ([]Result, error) {
+	results := make([]Result, len(handlers))
+	if limit < 1 {
+		return results, errors.New("Limit must be greater than 0")
+	}
+	if len(handlers) < 1 {
+		return results, nil
+	}
+	out := make(chan Result)
+	in := make(chan Task, limit)
+	for index, t := range handlers {
+		go func(index int, t Task) {
+			in <- t
+			t(func(err error) {
+				if results[index].done == false {
+					out <- Result{Index: index, Err: err, done: true}
+					<-in
+				}
+			})
+		}(index, t)
+	}
+	for res := range out {
+		results[res.Index] = res
+		if hasEnded(results) {
+			close(out)
+			close(in)
+		}
+	}
+	return results, nil
 }
 
 // Run all provided tasks in a parallel fashion.
-func Parallel (handlers []Task) []Result {
-  results := make([]Result, len(handlers))
-  if len(handlers)<1 {
-    return results
-  }
-  out := make(chan Result)
-  for index, t := range handlers {
-    go func (index int, t Task) {
-      t(func (err error) {
-        if results[index].done==false {
-          out <- Result{Index: index, Err: err, done: true}
-        }
-      })
-    }(index, t)
-  }
-  for res := range out {
-    results[res.Index] = res
-    if hasEnded(results) {
-      close(out)
-    }
-  }
-  return results
+func Parallel(handlers []Task) []Result {
+	results := make([]Result, len(handlers))
+	if len(handlers) < 1 {
+		return results
+	}
+	out := make(chan Result)
+	for index, t := range handlers {
+		go func(index int, t Task) {
+			t(func(err error) {
+				if results[index].done == false {
+					out <- Result{Index: index, Err: err, done: true}
+				}
+			})
+		}(index, t)
+	}
+	for res := range out {
+		results[res.Index] = res
+		if hasEnded(results) {
+			close(out)
+		}
+	}
+	return results
 }
-func hasEnded (results []Result) bool {
-  ended := true
-  for _, res := range results {
-    ended = ended && res.done
-  }
-  return ended
+func hasEnded(results []Result) bool {
+	ended := true
+	for _, res := range results {
+		ended = ended && res.done
+	}
+	return ended
 }
 
 // func helper to tell if any Result failed with an error.
 func HasErrors(results []Result) bool {
-  h := false
-  for _,res := range results {
-    h = h || res.Err!=nil
-  }
-  return h
+	h := false
+	for _, res := range results {
+		h = h || res.Err != nil
+	}
+	return h
 }
--- async_test.go
+++ async_test.go
@@ -1,157 +1,155 @@
 package async
 
 import (
-  "errors"
-  "testing"
-  "fmt"
-  "time"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
 )
 
 func TestParallel(t *testing.T) {
-  ran := 0
-  results := Parallel([]Task{
-    func (done Done) {
-      ran+=1
-      done(nil)
-    },
-    func (done Done) {
-      ran+=1
-      done(nil)
-    },
-  })
+	ran := 0
+	results := Parallel([]Task{
+		func(done Done) {
+			ran += 1
+			done(nil)
+		},
+		func(done Done) {
+			ran += 1
+			done(nil)
+		},
+	})
 
-  if (len(results)!=2) {
-    t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 2)
-  }
-  if (ran!=2) {
-    t.Errorf("Expected to execute two func only, executed %d\n", ran)
-  }
+	if len(results) != 2 {
+		t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 2)
+	}
+	if ran != 2 {
+		t.Errorf("Expected to execute two func only, executed %d\n", ran)
+	}
 }
 
 func TestParallelErrored(t *testing.T) {
-  results := Parallel([]Task{
-    func (done Done) {
-      done(errors.New("42"))
-    },
-    func (done Done) {
-      done(nil)
-    },
-  })
+	results := Parallel([]Task{
+		func(done Done) {
+			done(errors.New("42"))
+		},
+		func(done Done) {
+			done(nil)
+		},
+	})
 
-  if (results[0].Err==nil) {
-    t.Errorf("Expected index 0 to have an error\n")
-  }
+	if results[0].Err == nil {
+		t.Errorf("Expected index 0 to have an error\n")
+	}
 }
 
 func TestParallelInvokeCallbackTwice(t *testing.T) {
-  results := Parallel([]Task{
-    func (done Done) {
-      done(nil)
-    },
-    func (done Done) {
-      done(nil)
-      done(nil)
-    },
-  })
+	results := Parallel([]Task{
+		func(done Done) {
+			done(nil)
+		},
+		func(done Done) {
+			done(nil)
+			done(nil)
+		},
+	})
 
-  if (results[1].Err!=nil) {
-    t.Errorf("Expected error of task #1 to be nil\n")
-  }
+	if results[1].Err != nil {
+		t.Errorf("Expected error of task #1 to be nil\n")
+	}
 }
 
 func TestParallelNoTasks(t *testing.T) {
-  results := Parallel([]Task{})
+	results := Parallel([]Task{})
 
-  if (len(results)!=0) {
-    t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 0)
-  }
+	if len(results) != 0 {
+		t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 0)
+	}
 }
 
-
-
 func TestParallelLimit(t *testing.T) {
-  ran := 0
-  results, err := ParallelLimit(1, []Task{
-    func (done Done) {
-      ran+=1
-      done(nil)
-    },
-    func (done Done) {
-      ran+=1
-      done(nil)
-    },
-  })
+	ran := 0
+	results, err := ParallelLimit(1, []Task{
+		func(done Done) {
+			ran += 1
+			done(nil)
+		},
+		func(done Done) {
+			ran += 1
+			done(nil)
+		},
+	})
 
-  if (len(results)!=2) {
-    t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 2)
-  }
-  if (ran!=2) {
-    t.Errorf("Expected to execute two func only, executed %d\n", ran)
-  }
-  if (err!=nil) {
-    t.Errorf("Expected err to be nil, got %q", err)
-  }
+	if len(results) != 2 {
+		t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 2)
+	}
+	if ran != 2 {
+		t.Errorf("Expected to execute two func only, executed %d\n", ran)
+	}
+	if err != nil {
+		t.Errorf("Expected err to be nil, got %q", err)
+	}
 }
 
 func TestParallelLimitErrored(t *testing.T) {
-  results, err := ParallelLimit(2, []Task{
-    func (done Done) {
-      done(errors.New("42"))
-    },
-    func (done Done) {
-      done(nil)
-    },
-  })
+	results, err := ParallelLimit(2, []Task{
+		func(done Done) {
+			done(errors.New("42"))
+		},
+		func(done Done) {
+			done(nil)
+		},
+	})
 
-  if (results[0].Err==nil) {
-    t.Errorf("Expected index 0 to have an error\n")
-  }
-  if (err!=nil) {
-    t.Errorf("Expected err to be nil, got %q", err)
-  }
+	if results[0].Err == nil {
+		t.Errorf("Expected index 0 to have an error\n")
+	}
+	if err != nil {
+		t.Errorf("Expected err to be nil, got %q", err)
+	}
 }
 
 func TestParallelLimitInvokeCallbackTwice(t *testing.T) {
-  results, err := ParallelLimit(2, []Task{
-    func (done Done) {
-      done(nil)
-    },
-    func (done Done) {
-      done(nil)
-      done(nil)
-    },
-  })
+	results, err := ParallelLimit(2, []Task{
+		func(done Done) {
+			done(nil)
+		},
+		func(done Done) {
+			done(nil)
+			done(nil)
+		},
+	})
 
-  if (results[1].Err!=nil) {
-    t.Errorf("Expected error of task #1 to be nil\n")
-  }
-  if (err!=nil) {
-    t.Errorf("Expected err to be nil, got %q", err)
-  }
+	if results[1].Err != nil {
+		t.Errorf("Expected error of task #1 to be nil\n")
+	}
+	if err != nil {
+		t.Errorf("Expected err to be nil, got %q", err)
+	}
 }
 
 func TestParallelLimitToZero(t *testing.T) {
-  _, err := ParallelLimit(0, []Task{
-    func (done Done) {
-      done(nil)
-    },
-    func (done Done) {
-      done(nil)
-    },
-  })
+	_, err := ParallelLimit(0, []Task{
+		func(done Done) {
+			done(nil)
+		},
+		func(done Done) {
+			done(nil)
+		},
+	})
 
-  if (err==nil) {
-    t.Errorf("Expected err to be not nil")
-  }
+	if err == nil {
+		t.Errorf("Expected err to be not nil")
+	}
 }
 
 func TestParallelLimitNoTasks(t *testing.T) {
-  results, err := ParallelLimit(1, []Task{})
+	results, err := ParallelLimit(1, []Task{})
 
-  if (len(results)!=0) {
-    t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 0)
-  }
-  if (err!=nil) {
-    t.Errorf("Expected err to be nil")
-  }
+	if len(results) != 0 {
+		t.Errorf("Expected results to have same length as tasks got%d, expected %d\n", len(results), 0)
+	}
+	if err != nil {
+		t.Errorf("Expected err to be nil")
+	}
 }
